91883995ba90c06b761f7fa697d34fc27321dd51
amin <amin.roosta@outlook.com>
2021-12-03 18:02:39 +0100

    First commit

---
+++ content-script.js
@@ -0,0 +1,50 @@
+console.log("***********************");
+
+const getStats = () => {
+	const infoEl = document.querySelector("ytd-video-primary-info-renderer");
+	if (!infoEl) return null;
+
+	const viewsEl = infoEl.querySelector("ytd-video-view-count-renderer .view-count");
+	const likesEl = infoEl.querySelector('yt-formatted-string[aria-label~="likes"]');
+
+	if (!viewsEl || !likesEl) return null;
+
+	const views = viewsEl.innerText;
+	const likes = likesEl.getAttribute("aria-label");
+
+	return {
+		likes: likes.replace(/[a-z,\s+]/ig, '') | 0,
+		views: views.replace(/[a-z,\s+]/ig, '') | 0,
+		viewsEl,
+		likesEl,
+	};
+};
+
+function showStats({ likes, views, likesEl }) {
+	const percentage = (100 * likes/views).toFixed(2);
+	let span = likesEl.querySelector('span');
+	if(!span) {
+		span = document.createElement('span');
+		span.style['padding-left'] = '1em';
+		likesEl.appendChild(span);
+	}
+	span.innerText = `(${percentage})%`
+}
+
+let holdBackTimer = null;
+function hodlBack() {
+	if (window.location.href.indexOf("watch?") >= 0) {
+		clearInterval(holdBackTimer);
+		holdBackTimer = setInterval(() => {
+			const stats = getStats();
+			if (stats) {
+				clearInterval(holdBackTimer);
+				holdBackTimer = null;
+				showStats(stats);
+			}
+		}, 150);
+	}
+}
+
+hodlBack();
+document.addEventListener("yt-navigate-finish", hodlBack);
---
+++ readme.md
@@ -0,0 +1,10 @@
+### youtube like to view ratio
+
+Youtube recently removed the dislike to like ratio.  
+Usually 5% of viewers like a video, this extension shows that ratio.
+
+TODO: add chrome extension link
+TODO: add firefox extension link
+
+![(like to view)%](./assets/readme.png)
+
