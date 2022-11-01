39c7ad6812e66df9c89d8de6ebc6cb7c74533e8c
amin <amin.roosta@outlook.com>
2022-11-01 18:04:00 -0400

    upgrade to new youtube style

--- content-script.js
+++ content-script.js
@@ -1,18 +1,13 @@
 const getStats = () => {
-	const infoEl = document.querySelector("ytd-video-primary-info-renderer");
-	if (!infoEl) return null;
-
-	const viewsEl = infoEl.querySelector("ytd-video-view-count-renderer .view-count");
-	const likesEl = infoEl.querySelector("ytd-menu-renderer yt-formatted-string[aria-label]");
+	const likesEl = document.querySelector('[aria-label^="like this video along with"]') || document.querySelector('[aria-label$="likes"]') ;
+	const viewsEl = document.querySelector('#above-the-fold #description-inner tp-yt-paper-tooltip #tooltip');
 
 	if (!viewsEl || !likesEl) return null;
 
-	const views = viewsEl.innerText;
+	const views = viewsEl.innerText.split("views")[0];
 	const likes = likesEl.getAttribute("aria-label");
 
-
     if (views.includes("watching now")) return null;
-
 	return {
 		likes: likes.replace(/\D/g, '') * 1,
 		views: views.replace(/\D/g, '') * 1,
@@ -21,13 +16,18 @@ const getStats = () => {
 	};
 };
 
-function showStats({ likes, views, likesEl }) {
+function showStats({ likes, views }) {
 	const percentage = (100 * likes / views).toFixed(2);
-	let span = likesEl.querySelector('span');
+	const likeDiv = document.querySelector('#top-row #actions #segmented-like-button yt-button-shape button div:nth-child(2)');
+	let span = likeDiv.querySelector('span.yt-like-to-view-ratio')
 	if (!span) {
 		span = document.createElement('span');
+        span.classList.add('yt-like-to-view-ratio');
 		span.style['padding'] = '0 0.6em';
-		likesEl.prepend(span);
+		span.style['font-weight'] = 'bold';
+		likeDiv.append(span);
+
+        likeDiv.parentElement.addEventListener('click', () => showStats({likes, views}));
 	}
 	span.innerText = `(${percentage}%)`
 }
@@ -38,6 +38,7 @@ function hodlBack() {
 		clearInterval(holdBackTimer);
 		holdBackTimer = setInterval(() => {
 			const stats = getStats();
+
 			if (stats) {
 				clearInterval(holdBackTimer);
 				holdBackTimer = null;
